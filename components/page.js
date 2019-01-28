ddfe6d94569e0bae0c087d475e793cfe8c65dd96
Tony Spiro <tspiro@tonyspiro.com>
2019-01-27 21:06:12 -0800

    using

--- components/page.js
+++ components/page.js
@@ -38,7 +38,7 @@ export default ({ page }) => {
 						<div style={description_style}>
 							<div style={{ marginBottom: 80, fontSize: 30 }}>Use &larr;&rarr; keys to navigate slides</div>
 							<div style={{ marginBottom: 20 }}><a style={{ color: '#fff', textDecoration: 'none' }} href="https://cosmicjs.com?ref=fyre-starter" target="_blank">Click here to create your own 🔥 fundraising presentation</a>.</div>
-							<div>Made with 😂 by <a style={{ color: '#fff', textDecoration: 'none' }} href="https://cosmicjs.com?ref=fyre-starter" target="_blank">Cosmic JS</a>.</div>
+							<div>Made with 😂 using <a style={{ color: '#fff', textDecoration: 'none' }} href="https://cosmicjs.com?ref=fyre-starter" target="_blank">Cosmic JS</a>.</div>
 						</div>
 					: ''
 				}
